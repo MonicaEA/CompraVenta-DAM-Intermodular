586ce8ecc883a14f0acd5e548f90243ef1226e75
Mónica Espiñeira <monica.espineira1988@gmail.com>
2026-04-25 11:53:12 +0200

    Correccion en enlace con javaFX

---
+++ src/RetroJuegos/src/main/java/com/retrojuegos/retrojuegos/ControllerView/VentasViewController.java
@@ -0,0 +1,39 @@
+package com.retrojuegos.retrojuegos.ControllerView;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class VentasViewController {
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private Button btnFinalizar;
+
+    @FXML
+    private Button btnVolver;
+
+    @FXML
+    private Label lblTotal;
+
+    @FXML
+    private TableView<?> tablaCarrito;
+
+    @FXML
+    private TableView<?> tablaCatalogo;
+
+    @FXML
+    private TextField txtBuscar;
+
+
+
+
+
+
+
+
+}
